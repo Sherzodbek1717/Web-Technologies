0c97c2d531388970fd17591e1fc1688f1bc7db18
Sherzodbek1717 <wiut13457@gmail.com>
2023-03-20 16:12:18 +0500

    Some minor changes

---
+++ README.md
@@ -0,0 +1,24 @@
+# TO DO APPLICATION 
+
+This app helps users to track the to dos.
+
+#### Instructions 
+1.Download the source code 
+
+```bash
+git clone [URL TO GITHUB REPO]
+```
+or just download the *.zip file 
+
+2.Install deps 
+```bash
+npm install
+```
+3.Run the app
+```bash
+node app
+```
+#### Deps list 
+
+- express 
+- pug
--- app.js
+++ app.js
@@ -1,19 +1,69 @@
+// third party libs 
 const express = require('express')
 const app = express()
-const PORT = 8000
+app.use(express.urlencoded({ extended: false }))
 
+
+// node libs 
+const fs = require('fs')
+const PORT = 8080
 
 app.set('view engine', 'pug')
+app.set('views', './views')
+app.use('/static', express.static('public')) // assets
+app.use(express.urlencoded({ extended: false}))
 
-// https://localhost:8000
+// localhost:8080
 app.get('/', (req, res) => {
-    res.render('home')
+    fs.readFile('./data/todos.json', (err, data) =>{
+        if (err) throw err
+        const todos = JSON.parse(data)
+
+        res.render('home', {todos: todos})
+    })
 })
 
+app.post('/Add', (req, res) => {
+    const formData = req.body
 
+    if (formData.todo.trim() == '') {
+        fs.readFile('./data/todos.json', (err, data) =>{
+            if (err) throw err 
+            const todos = JSON.parse(data)
+            res.render('home', {error: true, todos: todos})
 
+        })
+    }
+    else {
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+            
+            const todos = JSON.parse(data)
+
+            const todo = {
+                id: id(), 
+                description: formData.todo,
+                done: false 
+            }
+
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err 
+
+                res.redirect('/')
+            })
+        })
+    }
+})
 
 app.listen(PORT, (err) => {
-    if (err) throw errow
-    console.log('This app is running on port' + PORT );
+    if (err) throw err
+    console.log('This app is running on port ' + PORT)
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2.9);
+}
+
+
